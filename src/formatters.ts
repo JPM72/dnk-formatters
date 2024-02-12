87fdc04a9ddadf06872dcb412de8bdeda703beea
Johan Meyer <35699550+JPM72@users.noreply.github.com>
2024-02-12 08:35:01 +0200

    number formatters fixes

--- src/formatters.ts
+++ src/formatters.ts
@@ -1,11 +1,11 @@
-import { FIGURE_SPACE, OrdinalSuffix } from './util'
+import { FIGURE_SPACE, OrdinalSuffix, flowPipe } from './util'
 import { numberFormatter } from './number'
 import { stringFormatters } from './string'
 import { dateFormatter } from './date'
 import { config, configure } from './configure'
 
 export const formatters = {
-	FIGURE_SPACE,
+	FIGURE_SPACE, flowPipe,
 	ordinal: OrdinalSuffix,
 	number: numberFormatter,
 	string: stringFormatters,
